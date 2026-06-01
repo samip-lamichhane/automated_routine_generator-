9ae00d59b4141ff908095d58b6b56748c94cb829
Prabesh <poudelprabesh100@gmail.com>
2026-06-01 09:24:30 +0545

    error fix

--- classSession.hpp
+++ classSession.hpp
@@ -6,17 +6,17 @@
 class ClassSession{
 private:
 TimeSlot m_timeslot;
-string m_teacherid;
-string m_subjectid;
-string m_roomid;
-string m_batchid;
+std::string m_teacherid;
+std::string m_subjectid;
+std::string m_roomid;
+std::string m_batchid;
 public:
 ClassSession(TimeSlot timeslot,std::string teacherid,std::string subjectid,std::string roomid,std::string batchid );
 TimeSlot getTimeSlot() const;
-string getTeacherId() const;
-string getSubjectId() const;
-string getRoomId() const;
-string getBatchId() const;
+std::string getTeacherId() const;
+std::string getSubjectId() const;
+std::string getRoomId() const;
+std::string getBatchId() const;
 
 
 
